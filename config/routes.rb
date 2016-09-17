5c425d6a9f5e81c9bc4a37025109192541bdb8c1
Ryan Rich <rrichrs@gmail.com>
2016-09-17 10:56:04 -0700

    Updating root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  root 'application#hello'
+    # Redirect example
+    # get '/h', to: 'static_pages#home'
+    root 'static_pages#home'
 end
