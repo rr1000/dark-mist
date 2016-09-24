5aece8bf363df106125b83d3991808888f4a023d
Ryan Rich <rrichrs@gmail.com>
2016-09-23 19:27:38 -0700

    I don't know really

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(page_title = '')
+        base_title = "Circumstellar Disk"
+        if page_title.empty?
+            base_title
+        else
+            page_title + " | " + base_title
+        end
+    end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,12 @@
 require 'test_helper'
+
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-    test "should get root" do
-        get root_url
-        assert_response :success
-        assert_select "title", "Home"
+    def setup
+        @base_title = "Circumstellar Disc"
     end
-    # test "should get home" do
-    #     get static_pages_home_url
-    #     assert_response :success
-    #     assert_select "title", "Home"
-    # end
+    test "should get home" do
+        get static_pages_home_url
+        assert_response :success
+        assert_select "title", "Home | #{@base_title}"
+    end
 end
