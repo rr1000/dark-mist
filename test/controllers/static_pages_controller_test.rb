8a103c527d4bdb22ea624a1fa9ddc352fede1372
Ryan Rich <rrichrs@gmail.com>
2016-09-23 19:46:10 -0700

    idk

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,12 +1,9 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-    def setup
-        @base_title = "Circumstellar Disc"
-    end
-    test "should get home" do
-        get static_pages_home_url
-        assert_response :success
-        assert_select "title", "Home | #{@base_title}"
-    end
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home | Circumstellar Disc"
+  end
 end
